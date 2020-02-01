c2a7c9b67713773b59a1ccda1ad740cb78e49d0e
zamharrad <mhmdzamhar@gamil.com>
2020-02-01 09:44:42 +0530

    Realationship mapping

--- src/main/java/com/rad/recipe/model/Recipe.java
+++ src/main/java/com/rad/recipe/model/Recipe.java
@@ -31,6 +31,8 @@ public class Recipe {
     private Set<Ingredient> ingredients;
 
     @ManyToMany
+    @JoinTable(name = "recipe_category",joinColumns = @JoinColumn(name = "recipe_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories;
 
     public Long getId() {

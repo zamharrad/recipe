86672c2a9463531cd36bc20ced0ce17c0ecd999e
zamharrad <mhmdzamhar@gamil.com>
2020-02-01 09:29:39 +0530

    commit realationship mapping and add catogory domain

--- src/main/java/com/rad/recipe/model/Recipe.java
+++ src/main/java/com/rad/recipe/model/Recipe.java
@@ -17,7 +17,9 @@ public class Recipe {
     private String url;
     private String direction;
     //todo
-    //private Difficulty difficulty;
+
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
 
     @Lob
     private Byte[] image;
@@ -27,6 +29,9 @@ public class Recipe {
 
     @OneToMany(cascade = CascadeType.ALL,mappedBy = "recipe")
     private Set<Ingredient> ingredients;
+
+    @ManyToMany
+    private Set<Category> categories;
 
     public Long getId() {
         return id;
@@ -114,5 +119,21 @@ public class Recipe {
 
     public void setIngredients(Set<Ingredient> ingredients) {
         this.ingredients = ingredients;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 }
